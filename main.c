cdb9fd1b4eac3ff1fbd00974ffb4cd1970495014
pavelrosh <pavelrosh@gmail.com>
2018-02-14 22:07:16 +0200

    beta scaling

--- main.c
+++ main.c
@@ -17,7 +17,43 @@
 // 	mlx_string_put(d->mlx, d->win, 100, 100, 0x00cc00, "move       : arrows");
 // 	return (0);
 // }
+// int 	scaling(t_mlx *d)
+// {
+// 	int k;
+// 	double res_x;
+// 	double res_y;
+// 	double res_z;
+// 	int res;
 
+// 	k = -1;
+// 	res = 1;
+// 	res_x = width / (d->coords[d->el_num - 1].x - d->coords[0].x);
+// 	res_y = height / (d->coords[d->el_num - 1].y - d->coords[0].y);
+	
+// 	if (res_x < res_y)
+// 		res = res_x / 2;
+// 	else
+// 		res = res_y / 2;
+// 	return (res);
+// }
+
+// void 	isometric(t_mlx *d)
+// {
+// 	ft_rotate(d, 'x', (-50 * 3.14) / 180);
+// 	ft_rotate(d, 'y', (-30 * 3.14) / 180);
+// 	ft_rotate(d, 'z', (-15 * 3.14) / 180);
+// }
+
+// void	init_data(t_mlx *d)
+// {
+// 	d->mlx = mlx_init();
+// 	d->win = mlx_new_window(d->mlx, width, height, "Fdf");
+// 	isometric(d);
+// 	in_center(d);
+// 	ft_zoom(d, scaling(d));
+// 	mlx_hook(d->win, 2, 5, key_hook, d);
+// 	mlx_loop(d->mlx);
+// }
 void 	start_coord(t_mlx *d)
 {
 	int k;
@@ -60,6 +96,7 @@ int		main(int argc, char **argv)
 			coord_in_arr(d, xy);
 			d->mlx = mlx_init();
 			d->win = mlx_new_window(d->mlx, width, height, "Fdf");
+			// isometric(d);
 			in_center(d);
 			ft_zoom(d, 35);
 			start_coord(d);
